36c96b5fd1b20a5e428f6f5647457c793824338e
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-10-03 15:27:21 -0400

    Updates

---
+++ precache-manifest.5b3e1cfd09ca9b585f2c7ae07e43fe21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3237c6b92de5aa546a1ba44881877973",
+    "url": "/lab-wiki-countries/index.html"
+  },
+  {
+    "revision": "6bc3144dde0c56200a2c",
+    "url": "/lab-wiki-countries/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "8667f00d5472911b8472",
+    "url": "/lab-wiki-countries/static/css/main.ea35f720.chunk.css"
+  },
+  {
+    "revision": "6bc3144dde0c56200a2c",
+    "url": "/lab-wiki-countries/static/js/2.71584eb0.chunk.js"
+  },
+  {
+    "revision": "8667f00d5472911b8472",
+    "url": "/lab-wiki-countries/static/js/main.36ebbb56.chunk.js"
+  },
+  {
+    "revision": "29fd73df3c8f8342b740",
+    "url": "/lab-wiki-countries/static/js/runtime-main.50467aaa.js"
+  }
+]);
