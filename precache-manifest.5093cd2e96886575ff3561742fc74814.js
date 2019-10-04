d5d5eb61b6ef687e6c13ccc8d6b87139053bc81c
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-10-04 10:04:03 -0400

    Updates

--- precache-manifest.5093cd2e96886575ff3561742fc74814.js
+++ precache-manifest.5093cd2e96886575ff3561742fc74814.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "54ec5ae8b66b0f8c2bc23beac35671a0",
+    "revision": "fd28227cb880082095a381685061f8f6",
     "url": "/lab-wiki-countries/index.html"
   },
   {
-    "revision": "76521c8aa06db9351b64",
+    "revision": "6bc3144dde0c56200a2c",
     "url": "/lab-wiki-countries/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "6283779e72efce4f9c68",
+    "revision": "32da5bd58824b9bbb0e8",
     "url": "/lab-wiki-countries/static/css/main.ea35f720.chunk.css"
   },
   {
-    "revision": "76521c8aa06db9351b64",
-    "url": "/lab-wiki-countries/static/js/2.63cc6933.chunk.js"
+    "revision": "6bc3144dde0c56200a2c",
+    "url": "/lab-wiki-countries/static/js/2.71584eb0.chunk.js"
   },
   {
-    "revision": "6283779e72efce4f9c68",
-    "url": "/lab-wiki-countries/static/js/main.35c4c1a2.chunk.js"
+    "revision": "32da5bd58824b9bbb0e8",
+    "url": "/lab-wiki-countries/static/js/main.e11fb54b.chunk.js"
   },
   {
     "revision": "29fd73df3c8f8342b740",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lab-wiki-countries/precache-manifest.724fee72ab77b68dc77dd763a820d3b0.js"
+  "/lab-wiki-countries/precache-manifest.5093cd2e96886575ff3561742fc74814.js"
 );
 
 self.addEventListener('message', (event) => {
